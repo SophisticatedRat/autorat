cc6dc075af2d52c8d5a9e42bd5e2d88d32589d32
SeraphAranda <seraphagames@gmail.com>
2024-11-12 07:43:32 -0700

    Copy and paste, man

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,4 +8,80 @@ describe('My Login application', () => {
         await LoginPage.login('standard_user', 'secret_sauce')
     })
 })
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
 
+        await LoginPage.login('standard_user', 'secret_source')
+    })
+})
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('locked_out_user', 'secret_sauce')
+    })
+})
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('locked_out_user', 'secret_source')
+    })
+})
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('problem_user', 'secret_sauce')
+    })
+})
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('problem_user', 'secret_source')
+    })
+})
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('performance_glitch_user', 'secret_sauce')
+    })
+})
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('performance_glitch_user', 'secret_source')
+    })
+})
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('error_user', 'secret_sauce')
+    })
+})
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('error_user', 'secret_source')
+    })
+})
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('visual_user', 'secret_sauce')
+    })
+})
+describe('False Login application', () => {
+    it('should not login with valid credentials', async () => {
+        await LoginPage.site()
+
+        await LoginPage.login('visual_user', 'secret_source')
+    })
+})
